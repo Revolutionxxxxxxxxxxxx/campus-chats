7c67e31bb94fa512e3b024b384a9e554276178f5
Revolutionxxxxxxxxxxxx <137689604+Revolutionxxxxxxxxxxxx@users.noreply.github.com>
2026-08-08 22:11:36 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAITbFcHt9IqrcLSjraufWpGc86ONCyGQs",
+    authDomain: "campus-chat-7c9c5.firebaseapp.com",
+    projectId: "campus-chat-7c9c5",
+    storageBucket: "campus-chat-7c9c5.firebasestorage.app",
+    messagingSenderId: "799567513368",
+    appId: "1:799567513368:web:cded50041ab08a7bb4c06d",
+    measurementId: "G-YNBZFJHFDS"
+  };
